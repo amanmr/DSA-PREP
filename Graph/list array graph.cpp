737bd866424cbef95a2fd9f58f4e68ea54e68ef5
Aman Mishra <iamamanmr@gmail.com>
2022-11-15 23:49:29 +0530

    simple graph using array of list

---
+++ Graph/list array graph.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<list>
+using namespace std;
+class graph{
+    int v;
+    list<int> *l;
+    public:
+    graph(int v){
+        this->v=v;
+        l=new list<int> [v];
+    }
+    void addEdge(int x,int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+    void printEdge(){
+        for(int i=0;i<v;i++){
+            cout<<i<<"--> ";
+            for(int x:l[i]){
+                cout<<x<<" ,";
+            }
+            cout<<endl;
+        }
+    }
+};
+int main(){
+
+    graph g(4);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(0,2);
+    g.addEdge(2,3);
+    g.printEdge();
+
+    return 0;
+}
